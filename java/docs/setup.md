c4a665f7b0f782023fcb1c8bd52f31826d039a6b
DEV <dev@dev.com>
2021-11-22 12:59:16 +0530

    commit..

--- java/docs/setup.md
+++ java/docs/setup.md
@@ -1,2 +1,5 @@
 :link: [mysql-installer-community-8.0.27.1](https://dev.mysql.com/get/Downloads/MySQLInstaller/mysql-installer-community-8.0.27.1.msi)  
 :link: [mysql library](https://repo1.maven.org/maven2/mysql/mysql-connector-java/8.0.27/mysql-connector-java-8.0.27.jar)
+
+:boom: Bugs  
+:link: [Fix-Error-2502-and-2503-while-installing-or-uninstalling mysql on windows 10](https://kb.foxitsoftware.com/hc/en-us/articles/360040660391-Fix-Error-2502-and-2503-while-installing-or-uninstalling)
--- java/ld-java-master/src/dem/jdbc/Select.java
+++ java/ld-java-master/src/dem/jdbc/Select.java
@@ -2,16 +2,30 @@ package dem.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Select {
 	public static void main(String[] args) {
 		try {
+			System.out.println("CONNECTING");
 			Connection mySQLConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/JBEDB", "root",
 					"mysql");
-			System.out.println("CONNECTION SUCCESSFUL");
+			System.out.println("CREATING STATEMENT");
+			Statement selectStatement = mySQLConnection.createStatement();
+			System.out.println("EXECUTING QUERY AND OBTAINING RESULTSET");
+			ResultSet resultSet =  selectStatement.executeQuery("SELECT * FROM PRODUCTS");
+			System.out.println("ITERATING THROUGH THE RESULT SET AND FETCHING THE ROWS");
+			while(resultSet.next()) {
+				int productId = resultSet.getInt("PRODUCT_ID");
+				String productName = resultSet.getString("PRODUCT_NAME");
+				float price = resultSet.getFloat("PRICE");
+				System.out.println(productId + "  >  "+productName+"  >  "+price);
+			}
 		} catch (SQLException sqlException) {
 			System.out.println("DATABASE CONNECTION ISSUE " + sqlException);
+			
 		}
 	}
 }
