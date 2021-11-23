e610a02cc1749ea66fd0d7bfab9b745f173163e9
DEV <dev@dev.com>
2021-11-23 09:15:06 +0530

    commit..

--- java/ld-java-master/src/dem/jdbc/ProductApp.java
+++ java/ld-java-master/src/dem/jdbc/ProductApp.java
@@ -2,6 +2,7 @@ package dem.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
@@ -10,19 +11,20 @@ public class ProductApp {
 	public static void main(String[] args) {
 		System.out.println("MENU SKELETON");
 		ProductApp productApp = new ProductApp();
-		productApp.createProduct();
-
+//		productApp.createProduct();
+productApp.searchProduct();
 	}
-	public  void createProduct() {
+
+	public void createProduct() {
 //		CREATE A PRODUCT
-			Scanner scanner = new Scanner(System.in);
-			System.out.print("PID : ");
-			int productId = Integer.parseInt(scanner.nextLine());
-			System.out.print("\nNAME : ");
-			String productName = scanner.nextLine();
-			System.out.print("\nPRICE : ");
-			float price = Float.parseFloat(scanner.nextLine());
-			
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("PID : ");
+		int productId = Integer.parseInt(scanner.nextLine());
+		System.out.print("\nNAME : ");
+		String productName = scanner.nextLine();
+		System.out.print("\nPRICE : ");
+		float price = Float.parseFloat(scanner.nextLine());
+
 		try {
 			System.out.println("CONNECTING");
 			Connection mySQLConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/JBEDB", "root",
@@ -34,14 +36,59 @@ public class ProductApp {
 			String queryString = "INSERT INTO PRODUCTS VALUES (" + productId + ",'" + productName + "'," + price + ")";
 			System.out.println(queryString);
 			int rowCount = insertStatement.executeUpdate(queryString);
-			if(rowCount!=0) {
+			if (rowCount != 0) {
 				System.out.println("Inserted : " + rowCount);
-			}
-			else {
+			} else {
 				System.out.println("No Rows Inserted!");
 			}
 		} catch (SQLException sqlException) {
-			System.out.println("DATABASE CONNECTION ISSUE " + sqlException);		
+			System.out.println("DATABASE CONNECTION ISSUE " + sqlException);
 		}
+	}
+
+	public void searchProduct() {
+//		SEARCH BY PRODUCT ID OR PRODUCT NAME
+		Scanner scanner = new Scanner(System.in);
+//		System.out.print("PID : ");
+//		int productId = Integer.parseInt(scanner.nextLine());
+		System.out.print("\nNAME : ");
+		String productName = scanner.nextLine();
+
+		try {
+			Statement selectStatement = getStatement();
+//			Executes the given SQL statement, which may be an INSERT, UPDATE, or DELETE statement
+//			System.out.println("EXECUTING QUERY AND OBTAINING RESULTSET");
+			String queryString = "SELECT * FROM PRODUCTS WHERE PRODUCT_NAME = '"+ productName + "'";
+//			System.out.println(queryString);
+			ResultSet resultSet =  selectStatement.executeQuery(queryString);
+//			System.out.println("ITERATING THROUGH THE RESULT SET AND FETCHING THE ROWS");
+			boolean found = false;
+			while(resultSet.next()) {
+				found = true;
+				int productId = resultSet.getInt("PRODUCT_ID");
+				productName = resultSet.getString("PRODUCT_NAME");
+				float price = resultSet.getFloat("PRICE");
+				System.out.println(productId + "  >  "+productName+"  >  "+price);
+			}
+			if(!found) {
+				System.out.println("No products found....");
+			}
+		} catch (SQLException sqlException) {
+			System.out.println("DATABASE CONNECTION ISSUE " + sqlException);
+		}
+	}
+	
+	public Statement getStatement() {
+		try {
+//			System.out.println("CONNECTING");
+			Connection mySQLConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/JBEDB", "root",
+					"mysql");
+//			System.out.println("CREATING STATEMENT");
+			Statement statement = mySQLConnection.createStatement();
+			return statement;
+		} catch (SQLException sqlException) {
+			System.out.println("DATABASE CONNECTION ISSUE " + sqlException);
+		}
+		return null;
 	}
 }
