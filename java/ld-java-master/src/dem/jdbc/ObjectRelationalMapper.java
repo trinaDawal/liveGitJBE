f105a2e8d4807efe9469a555880532e75aba5953
DEV <dev@dev.com>
2021-11-23 11:52:37 +0530

    commit..

--- java/ld-java-master/src/dem/jdbc/ObjectRelationalMapper.java
+++ java/ld-java-master/src/dem/jdbc/ObjectRelationalMapper.java
@@ -1,8 +1,14 @@
 package dem.jdbc;
 
 import java.util.ArrayList;
+
 // LAB
 public class ObjectRelationalMapper {
+//	FUNCTION ACCEPTS QUERY STRING AS A PARAMERTER
+//  RETURNS A LIST OF PRODUCTS
+// SELECT * FROM PRODUCTS;
+// SELECT * FROM PRODUCTS WERHE PRODUCT_ID = 5;
+// SELECT * FROM PRODUCTS WERHE PRODUCT_NAME = 'DW';	
 	public static ArrayList<Product> getProducts(String queryString) {
 //	connect to db
 //	fetch each Product Row from ResultSet and add them to Product object.
