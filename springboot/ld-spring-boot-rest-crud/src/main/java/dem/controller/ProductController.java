c3686459599f8ddf4325b0400b629a2594c4e382
DEV <dev@dev.com>
2021-12-02 14:35:35 +0530

    commit..

--- springboot/ld-spring-boot-rest-crud/src/main/java/dem/controller/ProductController.java
+++ springboot/ld-spring-boot-rest-crud/src/main/java/dem/controller/ProductController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,10 +43,17 @@ public class ProductController {
 		productsHTML +=	 "</table></body></html>";
 		return productsHTML;
 	}
-
+// GET ALL THE PRODUCTS
 	@GetMapping("/products")
 	public List<Product> getAllProducts() {
 		List<Product> productsList = productRepository.findAll();
 		return productsList;
+	}
+//  CREATE A NEW PRODUCT
+	@PostMapping("/products")
+	public Product createProduct(@RequestBody Product productFromBrowser) {
+		System.out.println("Inserting : " + productFromBrowser);
+		Product savedProduct = productRepository.save(productFromBrowser);
+		return savedProduct;
 	}
 }
